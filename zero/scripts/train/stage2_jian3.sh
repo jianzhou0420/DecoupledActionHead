46c546ab8ba0904809572ad7ab79064471c35464
jian.zhou <jianzhou0420@outlook.com>
2025-06-16 23:33:46 +0930

    fix name

--- zero/scripts/train/stage2_jian3.sh
+++ zero/scripts/train/stage2_jian3.sh
@@ -47,7 +47,7 @@ for LETTER in $(echo "$INPUT_TASK_LETTERS" | sed -e 's/\(.\)/\1 /g'); do
     fi
 
     echo "Running trainer.py for task: '$LETTER' (Descriptive Name: $DESCRIPTIVE_TASK_NAME)"
-    python trainer_pl_stage2.py --config-name=DP_DecoupleActhonHead_stage2_ABC_D n_demo=1000 task_name="$DESCRIPTIVE_TASK_NAME" task.env_runner.n_envs=28
+    python trainer_pl_stage2.py --config-name=DP_DecoupleActionHead_stage2_ABC_D n_demo=1000 task_name="$DESCRIPTIVE_TASK_NAME" task.env_runner.n_envs=28
     echo "Finished trainer.py for task: '$LETTER'"
     echo "---"
 done

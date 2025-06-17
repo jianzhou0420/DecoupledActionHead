3464e3f3b9ada25b43396feba61f24c2eea11eac
jian.zhou <jianzhou0420@outlook.com>
2025-06-17 14:40:42 +0930

    1

--- zero/scripts/train/stage2_jian4.sh
+++ zero/scripts/train/stage2_jian4.sh
@@ -16,15 +16,15 @@ fi
 # ---
 declare -A TASK_MAP
 TASK_MAP["A"]="stack_d1"
-TASK_MAP["B"]="coffee_d2"
-TASK_MAP["C"]="three_piece_assembly_d2"
-TASK_MAP["D"]="stack_three_d1"
-TASK_MAP["E"]="square_d2"
-TASK_MAP["F"]="threading_d2"
-TASK_MAP["G"]="hammer_cleanup_d1"
+TASK_MAP["B"]="square_d2"
+TASK_MAP["C"]="coffee_d2"
+TASK_MAP["D"]="threading_d2"
+TASK_MAP["E"]="stack_three_d1"
+TASK_MAP["F"]="hammer_cleanup_d1"
+TASK_MAP["G"]="three_piece_assembly_d2"
 TASK_MAP["H"]="mug_cleanup_d1"
-TASK_MAP["I"]="kitchen_d1"
-TASK_MAP["J"]="nut_assembly_d0"
+TASK_MAP["I"]="nut_assembly_d0"
+TASK_MAP["J"]="kitchen_d1"
 TASK_MAP["K"]="pick_place_d0"
 TASK_MAP["L"]="coffee_preparation_d1"
 # Add more mappings as needed
@@ -48,7 +48,7 @@ for LETTER in $(echo "$INPUT_TASK_LETTERS" | sed -e 's/\(.\)/\1 /g'); do
     fi
 
     echo "Running trainer.py for task: '$LETTER' (Descriptive Name: $DESCRIPTIVE_TASK_NAME)"
-    python trainer_pl_stage2.py --config-name=DP_DecoupleActionHead_stage2_ABC_D n_demo=1000 task_name="$DESCRIPTIVE_TASK_NAME" task.env_runner.n_envs=28
+    python trainer_pl_stage2.py --config-name=DP_DecoupleActionHead_stage2_ABC_D n_demo=1000 task_name="$DESCRIPTIVE_TASK_NAME" task.env_runner.n_envs=28 task_alphabet=$LETTER
     echo "Finished trainer.py for task: '$LETTER'"
     echo "---"
 done

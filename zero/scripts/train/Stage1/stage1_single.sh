93a783a52dc4924eae7c26108d31fd78fdd984cc
jian.zhou <jianzhou0420@outlook.com>
2025-06-24 16:14:03 +0930

    1

--- zero/scripts/train/Stage1/stage1_single.sh
+++ zero/scripts/train/Stage1/stage1_single.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git stash
-git pull
+# git stash
+# git pull
 # ---
 # Check for provided task names
 # ---
@@ -48,7 +48,7 @@ for LETTER in $(echo "$INPUT_TASK_LETTERS" | sed -e 's/\(.\)/\1 /g'); do
     fi
 
     echo "Running trainer.py for task: '$LETTER' (Descriptive Name: $DESCRIPTIVE_TASK_NAME)"
-    python trainer_pl_all.py --config-name=DP_DecoupleActionHead_stage1 n_demo=1000 task_name="$DESCRIPTIVE_TASK_NAME" task.env_runner.n_envs=28 task_alphabet=$LETTER dataloader.num_workers=8 dataset_path="data/robomimic/datasets/$DESCRIPTIVE_TASK_NAME"/${DESCRIPTIVE_TASK_NAME}_abs_JP2eePose.hdf5
+    python trainer_pl_all.py --config-name=DP_DecoupleActionHead_stage1 n_demo=1000 task_name="$DESCRIPTIVE_TASK_NAME" task_alphabet=$LETTER dataloader.num_workers=16 dataset_path="data/robomimic/datasets/$DESCRIPTIVE_TASK_NAME"/${DESCRIPTIVE_TASK_NAME}_abs_JP2eePose.hdf5
     echo "Finished trainer.py for task: '$LETTER'"
     echo "---"
 done

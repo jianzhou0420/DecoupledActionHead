85d3ef92ac70ab20313533c4bb594f497a7bee31
jian.zhou <jianzhou0420@outlook.com>
2025-07-03 18:19:15 +0930

    1

--- trainer_pl_all.py
+++ trainer_pl_all.py
@@ -471,18 +471,21 @@ def train(cfg: AppConfig):
 
     if cfg.train_mode == 'stage1':
         callback_list = [checkpoint_callback,
+                         ActionMseLossForDiffusion(cfg),
                          ]
     elif cfg.train_mode == 'stage2':
         callback_list = [checkpoint_callback,
+                         ActionMseLossForDiffusion(cfg),
                          ]
 
     elif cfg.train_mode == 'normal':
         callback_list = [checkpoint_callback,
+                         ActionMseLossForDiffusion(cfg),
                          ]
     elif cfg.train_mode == 'stage2_rollout':
         rollout_callback_list = [RolloutCallback(cfg_env_runner[i], rollout_every_n_epochs=cfg.training.rollout_every) for i in range(len(cfg_env_runner))]
         callback_list = [checkpoint_callback,
-                         #  ActionMseLossForDiffusion(cfg),
+                         ActionMseLossForDiffusion(cfg),
                          ]
         callback_list.extend(rollout_callback_list)
     else:

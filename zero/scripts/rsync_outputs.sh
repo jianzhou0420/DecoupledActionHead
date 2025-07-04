c00c42b9812eca032b71c6aa0549e14b88c7f968
jian.zhou <jianzhou0420@outlook.com>
2025-07-04 15:51:32 +0930

    Exp A1000-A1000

---
+++ zero/scripts/rsync_outputs.sh
@@ -0,0 +1,4 @@
+rsync -avP jian@10.12.65.27:/home/jian/first/data/outputs/ /data/cache_outputs_from_others/jian1/
+rsync -avP jian@10.12.65.73:/data/first/data/outputs/ /data/cache_outputs_from_others/jian2/
+rsync -avP feng@10.12.65.120:/data/jian/first/data/outputs/ /data/cache_outputs_from_others/jian3/
+rsync -avP jian@10.12.119.67:/home/jian/first/data/outputs/ /data/cache_outputs_from_others/jian4/

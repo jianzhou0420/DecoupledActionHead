0f0ffcd198ed6376156cdb04d2b2126d1417ad97
jian.zhou <jianzhou0420@outlook.com>
2025-07-19 17:22:34 +0930

    new bash

--- zero/scripts/rsync_dataset.sh
+++ zero/scripts/rsync_dataset.sh
@@ -1,11 +1,9 @@
-rsync -avP --delete data/robomimic/datasets_abs/ jian@10.12.65.73:/data/first/data/robomimic/datasets_abs/
-rsync -avP --delete data/robomimic/datasets_abs/ feng@10.12.65.120:/data/jian/first/data/robomimic/datasets_abs/
-rsync -avP --delete data/robomimic/datasets_abs/ jian@10.12.119.67:/home/jian/first/data/robomimic/datasets_abs/
-rsync -avP --delete data/robomimic/datasets_abs/ jian@10.12.65.27:/home/jian/first/data/robomimic/datasets_abs/
-rsync -avP --delete data/robomimic/datasets_abs/ jian@10.12.65.130:/home/jian/first/data/robomimic/datasets_abs/
+rsync -avP --delete data/robomimic/datasets_abs/ jian@10.12.65.130:/home/jian/first/data/robomimic/datasets_abs/ #jian1
+rsync -avP --delete data/robomimic/datasets_abs/ jian@10.12.65.73:/data/first/data/robomimic/datasets_abs/       #jian2
+rsync -avP --delete data/robomimic/datasets_abs/ feng@10.12.65.120:/data/jian/first/data/robomimic/datasets_abs/ #jian3
+rsync -avP --delete data/robomimic/datasets_abs/ jian@10.12.119.67:/home/jian/first/data/robomimic/datasets_abs/ #jian4
 
+rsync -avP --delete data/robomimic/Stage1/ jian@10.12.65.130:/home/jian/first/data/robomimic/Stage1/
 rsync -avP --delete data/robomimic/Stage1/ jian@10.12.65.73:/data/first/data/robomimic/Stage1/
 rsync -avP --delete data/robomimic/Stage1/ feng@10.12.65.120:/data/jian/first/data/robomimic/Stage1/
 rsync -avP --delete data/robomimic/Stage1/ jian@10.12.119.67:/home/jian/first/data/robomimic/Stage1/
-rsync -avP --delete data/robomimic/Stage1/ jian@10.12.65.27:/home/jian/first/data/robomimic/Stage1/
-rsync -avP --delete data/robomimic/Stage1/ jian@10.12.65.130:/home/jian/first/data/robomimic/Stage1/

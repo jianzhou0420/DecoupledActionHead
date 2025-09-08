d6d10bd2efe71d4f17c2a85181f184fed17b3b24
jian.zhou <jianzhou0420@outlook.com>
2025-09-08 14:25:37 +0930

    1

--- zero/scripts/icra/DP_C/Exp_DP_C_Normal_multi.sh
+++ zero/scripts/icra/DP_C/Exp_DP_C_Normal_multi.sh
@@ -58,8 +58,7 @@ for LETTER in $(echo "$INPUT_TASK_LETTERS" | sed -e 's/\(.\)/\1 /g'); do
         seed=${SEED} \
         \
         task_alphabet=$LETTER \
-        train_mode=normal_rollout \
-        n_demo=1000 \
+        train_mode=normal \
         ckpt_path=${ckpt_path} \
         \
         dataloader.num_workers=16 \
